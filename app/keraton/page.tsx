8914010425b779d5130a97cc5d56545baed8c9c4
Enrico Pratama <enricopratama10@gmail.com>
2024-04-29 17:26:17 -0700

    resolved page keraton
    Co-authored-by: theophilaabigail <theophilaabigail@users.noreply.github.com>
    Co-authored-by: flooose01 <flooose01@users.noreply.github.com>
    Co-authored-by: arvinJson <arvinJson@users.noreply.github.com>

--- app/keraton/page.tsx
+++ app/keraton/page.tsx
@@ -3,16 +3,13 @@ import Performers from "./performers";
 import About from "./about";
 import Vendors from "./vendors";
 import Footer from "./footer";
-import BroughtBy from "./broughtBy";
 
 export const KeratonContent = () => {
-  // Your Keraton page content goes here
   return (
     <div className="max-h-max max-w-full mx-auto">
       <About />
-      <BroughtBy />
+
       <Performers />
-      {/* <Accordion /> */}
       <Vendors />
       <FAQ />
       <Footer />
