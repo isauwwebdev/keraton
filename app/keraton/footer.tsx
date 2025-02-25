b7b8f7b4bd7425cb91a3efc5ff73af5cf653d7b4
Enrico Pratama <enricopratama10@gmail.com>
2025-02-24 17:12:32 -0800

    changed year used in footer to dynamic

--- app/keraton/footer.tsx
+++ app/keraton/footer.tsx
@@ -63,6 +63,7 @@ const navigation = [
 ];
 
 export default function footer() {
+  const year = new Date().getFullYear();
   return (
     <footer className="bg-yellow">
       <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
@@ -80,7 +81,7 @@ export default function footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2024 Indonesian Student Association, UW, Inc. All rights
+            &copy; {year} Indonesian Student Association, UW, Inc. All rights
             reserved.
           </p>
         </div>
